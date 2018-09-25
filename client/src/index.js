aa7d4114b9fe868ca2b7823594fc293c1b1e5786
iMac <imac@iMac-de-iMac.lan>
2018-09-24 21:25:37 -0300

    Action Cable for production

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 //import registerServiceWorker from './registerServiceWorker';
 import { ActionCableProvider } from 'react-actioncable-provider';
-const API_WS_ROOT = 'ws://quiet-earth-27482.herokuapp.com:3001/cable';
+const API_WS_ROOT = 'ws://quiet-earth-27482.herokuapp.com/cable';
 
 ReactDOM.render(
   <ActionCableProvider url={API_WS_ROOT}>
